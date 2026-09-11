a027bdf2642fa95ca395ec55076cb31f7de8cb20
legend80s <liuchzong@qq.com>
2026-09-11 16:18:02 +0800

    fix(type): fix ts type error

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { parseArgs } from "node:util"
-// @ts-expect-error
 import { createLogger } from "walking-log"
 import { startServer } from "../backend/dev-server/index.js"
 import { openBrowser } from "../backend/utils/platform.js"
